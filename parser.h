3b79259a4c2a78915b8dd96e13852e7a59c9d7f2
Jonesma789 <47148836+Jonesma789@users.noreply.github.com>
2022-02-09 22:15:14 -0500

    Update parser.h

--- parser.h
+++ parser.h
@@ -3,44 +3,43 @@
 #include <string>
 #include "AST_NODES.h"
 
-AST_NODE_PROGRAM * program(); //done
+AST_NODE_PROGRAM * program(); 
 
-AST_NODE_FORWARD_DECLARE * forward_declare(); //done
-AST_NODE_IMPLEMENTATIONS * implementation(); //done
+AST_NODE_FORWARD_DECLARE * forward_declare();
+AST_NODE_IMPLEMENTATIONS * implementation(); 
 
-AST_NODE_FUNCTION_PROTO * function_proto(); //done
-AST_NODE_FUNCTION * function(); //done
+AST_NODE_FUNCTION_PROTO * function_proto(); 
+AST_NODE_FUNCTION * function(); 
 
 AST_NODE_PARAMETER_LIST * parameter_list();
-AST_NODE_VARIBALE_LIST * variable_list(); //done
-AST_NODE_DEFINITION * definition(); //done
-AST_NODE_TYPE * type(); //done
+AST_NODE_VARIBALE_LIST * variable_list();
+AST_NODE_DEFINITION * definition(); 
+AST_NODE_TYPE * type(); 
 
 
-AST_NODE_STATEMENT * statement(); //done
+AST_NODE_STATEMENT * statement(); 
 
-//types of statements
-AST_NODE_ASSIGNMENT * assignment(); //done
-AST_NODE_STATEMENT * ctrl_statement(); //done
-AST_NODE_STATEMENT *io_statement(); //done
+
+AST_NODE_ASSIGNMENT * assignment();
+AST_NODE_STATEMENT * ctrl_statement();
+AST_NODE_STATEMENT *io_statement(); 
 AST_NODE_FUNCTION_CALL *function_call();
-AST_NODE_RETURN * return_statement(); //done
+AST_NODE_RETURN * return_statement();
 
 
-//assignment statements and arithmetic
-AST_NODE_EXPR * expr(); //done
-AST_NODE_EXPR * term(); //done
-AST_NODE_EXPR * factor(); //done
-AST_NODE_EXPR * primary(); //done
-AST_NODE_ID * id(); //done
+AST_NODE_EXPR * expr();
+AST_NODE_EXPR * term();
+AST_NODE_EXPR * factor();
+AST_NODE_EXPR * primary();
+AST_NODE_ID * id();
 
-//Control statements
-AST_NODE_IF_STATEMENT * if_statement(); //done
-AST_NODE_STATEMENT * loop(); //done
 
-//loops
-AST_NODE_FOR_LOOP * for_loop(); //done
-AST_NODE_WHILE_LOOP * while_loop(); //done
+AST_NODE_IF_STATEMENT * if_statement(); 
+AST_NODE_STATEMENT * loop(); 
+
+
+AST_NODE_FOR_LOOP * for_loop();
+AST_NODE_WHILE_LOOP * while_loop(); 
 
 
 void error(std::string message);
